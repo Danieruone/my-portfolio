2012f6425477d783ade39082339633acb1b956e7
Daniel <danieruone@gmail.com>
2021-08-01 15:47:01 -0500

    images

--- src/components/CardProject/styles.js
+++ src/components/CardProject/styles.js
@@ -10,6 +10,9 @@ export const CardContainer = styled.div`
   margin: 20px;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
     0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  @media (max-width: 600px) {
+    margin: 20px 0 0 0;
+  }
 `;
 
 export const CardImage = styled.div`
