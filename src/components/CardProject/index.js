af97eb601f3194a65ac6adbb4bf99b67915e041a
Daniel <danieruone@gmail.com>
2021-07-31 21:48:54 -0500

    card project code button

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -1,20 +1,33 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 //styles
-import { CardContainer, CardImage, CardText, Button } from "./styles";
-import defaultImage from "../../images/default.jpg";
+import {
+  CardContainer,
+  CardImage,
+  CardText,
+  Button,
+  ButtonContainer,
+} from "./styles";
 
-export const CardProject = ({ image, name, description, url }) => {
+export const CardProject = ({ image, name, description, url, code_url }) => {
   return (
     <CardContainer>
       <CardImage>
-        <img src={image}></img>
+        <img src={image} alt="Imagen del proyecto" />
       </CardImage>
       <CardText>
         <h1>{name}</h1>
         <p>{description}</p>
-        <a href={url} target="_blank">
-          <Button>Ir al proyecto</Button>
-        </a>
+        <ButtonContainer>
+          <a href={url} target="_blank" rel="noreferrer">
+            <Button>Ir al proyecto</Button>
+          </a>
+          <a href={code_url} target="_blank" rel="noreferrer">
+            <Button>
+              <FaGithub /> Código
+            </Button>
+          </a>
+        </ButtonContainer>
       </CardText>
     </CardContainer>
   );
